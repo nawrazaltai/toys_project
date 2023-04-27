2d75df3908383a9d477457b757fcdeb48c7d85c7
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-04-27 13:44:32 +0200

    Registrering partly done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <button>
+            <li>
+              <Link href="/registrering">Register here</Link>
+            </li>
+          </button>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
