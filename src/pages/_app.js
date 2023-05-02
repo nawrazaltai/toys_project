518e7af9bf9bf4e2ebeeba5f9128734c402c999b
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-05-02 18:21:10 +0200

    signup nearly done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "../styles/signup.css";
 import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
@@ -11,7 +12,7 @@ export default function App({ Component, pageProps }) {
           </li>
           <button>
             <li>
-              <Link href="/registrering">Register here</Link>
+              <Link href="/signup">Sign up here</Link>
             </li>
           </button>
         </ul>
---
+++ src/pages/signup.js
@@ -0,0 +1,102 @@
+// det här är bara koden utan css och länk til db
+import { useState } from "react";
+
+export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  function handleSignUp() {
+    const user = {
+      username: username,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+    console.log("user: ", user);
+    fetch("", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => console.log("got response", res))
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <div class="all">
+      <logotype class="logo">
+        <img src="FooterLogo.png" />
+      </logotype>
+      <main class="signup">
+        <labe>Create your account</labe>
+        <br />
+        <br />
+        <input
+          placeholder="Username"
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          placeholder="First name"
+          type="text"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          placeholder="Last name"
+          type="text"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          placeholder="Email"
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          placeholder="Confirm password"
+          type="password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <label>
+          <input type="checkbox"></input>I agree with privacy and policy
+        </label>
+        <br />
+        <br />
+        <button onClick={handleSignUp}> Sign up</button>
+        <br />
+        <br />
+        <br />
+        <label>Already have an account</label>
+        <br />
+        <br />
+        <button>Sign in</button>
+      </main>
+    </div>
+  );
+}
