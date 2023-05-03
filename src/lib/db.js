250ca3d5a327524c3308be443147f37bb1d6686a
nawrazaltai <nawraz.altai@chasacademy.se>
2023-05-03 07:46:11 +0200

    database setup + users and products endpoints

---
+++ src/lib/db.js
@@ -0,0 +1,21 @@
+import mysql from "serverless-mysql";
+
+const db = mysql({
+  config: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    // ssl: { rejectUnauthorized: true }, // Kommentera bort vid uppkoppling mot localhost
+  },
+});
+export default async function excuteQuery({ query, values }) {
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } catch (error) {
+    console.log(error);
+    return { error };
+  }
+}
---
+++ src/pages/api/users.js
@@ -0,0 +1,28 @@
+import excuteQuery from "@/lib/db";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const sql = {
+      query: "SELECT * FROM users",
+    };
+
+    const result = await excuteQuery(sql);
+    res.status(200).json({ users: result });
+  }
+
+  if (req.method === "POST") {
+    const user = req.body;
+
+    const { username, firstName, lastName, email, password } = user;
+
+    console.log(user);
+    const sql = {
+      query:
+        "INSERT INTO users (username, first_name, last_name, email, password) VALUES (?,?,?,?,?)",
+      values: [username, firstName, lastName, email, password],
+    };
+
+    const result = await excuteQuery(sql);
+    res.status(200).json({ users: result });
+  }
+}
