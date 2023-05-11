64df7dce0041521195ada22974b5d798b8c37cd2
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-05-11 10:35:31 +0200

    Signup component completly done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,10 @@ import Link from "next/link";
 import { Lilita_One, Montserrat } from "next/font/google";
 
 const lilitaOne = Lilita_One({ weight: "400", subsets: ["latin"] });
-// import Signup from "./signup";
 
 export default function App({ Component, pageProps }) {
   return (
     <div className={lilitaOne.className}>
-      {/* <Signup /> */}
       <Component {...pageProps} />
     </div>
   );
