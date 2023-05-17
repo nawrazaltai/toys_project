3977d7e5b06807216bd92607295995f9197cd82a
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-05-17 13:56:13 +0200

    last commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,22 +2,22 @@ import mysql from "serverless-mysql";
 
 const db = mysql({
   config: {
-    host: "localhost", // process.env.DB_HOST,
-    database: "toys_project", // process.env.DB_DATABASE,
-    user: "root", // process.env.DB_USERNAME,
-    password: "", // process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     // port: 3306, // default port == 3306
     // ssl: { rejectUnauthorized: true }, // Kommentera bort vid uppkoppling mot localhost
   },
 });
 
 export default async function executeQuery({ query, values }) {
-    try {
-        const results = await db.query(query, values);
-        await db.end();
-        return results;
-    } catch (error) {
-        console.log("ERROR!", error);
-        return { error };
-    }
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } catch (error) {
+    console.log("ERROR!", error);
+    return { error };
+  }
 }
