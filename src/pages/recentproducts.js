b69830dd2198e57a645065a9923f3797214962f0
Fredrik Säfström <102657947+Naiso2022@users.noreply.github.com>
2023-05-17 08:51:54 +0200

    latest version

--- src/pages/recentproducts.js
+++ src/pages/recentproducts.js
@@ -3,19 +3,16 @@ import styles from "@/styles/RecentProducts.module.css";
 import { Inter, Lilita_One, Montserrat } from "next/font/google";
 import Link from "next/link";
 import Image from "next/image";
+// import ProductCard from "./productcard";
 import { useRouter } from "next/router";
-
 const PORT = 3000;
-
 export default function RecentProducts() {
     const router = useRouter();
     const query = router.query; /* states, user-data from loginForm */
     console.log("QUERY :: ", query.isLoggedIn)
-
     const [products, setProducts] = useState([]);
-
     async function FetchProducts() {
-        const response = await fetch(`http://localhost:${PORT}/api/products`, {
+        const response = await fetch("http://localhost:${PORT}/api/products", {
             //"https://planetscale-test-navy.vercel.app/api/products"
             method: "GET",
             mode: "cors",
@@ -27,29 +24,24 @@ export default function RecentProducts() {
         const jsonData = await response.json();
         setProducts(jsonData.products);
     }
-
     console.log(products, "PRODUCTS");
     const topThree = products.slice(0, 3);
     const topFour = products.slice(0, 4);
-
     useEffect(() => {
         FetchProducts();
     }, []);
-   
     return (
         <div className={styles.recent_products_container}>
-            {query.isLoggedIn && 
+            {query.isLoggedIn &&
                 <div className={styles.recent_products_greeting}>
                     <p>Welcome {query.username} to Rejoy!</p>
                 </div>
             }
-            
             <div className={styles.recent_products_title_div}>
                 <h2 className={styles.recent_products_h2}>
                     Most recently published donations
                 </h2>
             </div>
-
             {/* <div className={styles.recent_three_products_div}>
         {topThree.map((product) => {
           return <ProductCard product={product} />;
@@ -84,7 +76,6 @@ export default function RecentProducts() {
                     );
                 })}
             </div>
-
             {/* <div className={styles.recent_four_products_div}>
         {topFour.map((product) => {
           console.log(product);
@@ -120,72 +111,3 @@ export default function RecentProducts() {
         </div>
     );
 }
-
-const topThree = products.slice(0, 3);
-const topFour = products.slice(0, 4);
-
- useEffect(() => {
-FetchProducts();
-}, []);
-
-return (
-<div className={styles.recent_products_container}>
-<div className={styles.recent_products_title_div}>
-<h2 className={styles.recent_products_h2}>
-Most recently published donations
-</h2>
-</div>
-
-{/* <div className={styles.recent_three_products_div}>
-{topThree.map((product) => {
-return <ProductCard product={product} />;
-})}
-</div> */}
-<div className={styles.recent_three_products_div}>
-{topThree.map((product) => {
-return (
-<div className={styles.home_product_outer_border}>
-<article className={styles.home_product_article}>
-<Image
-width={2000}
-height={3000}
-src={product.url}
-alt={product.description}
-/>
- {/* <img src={product.url} alt={product.description} /> */}
-<h4 className={styles.home_product_title}>
- {product.product_title}
- </h4>
- <Link className={styles.home_product_link} href={"/"}>
-  View
-     </Link>
-  </article>
-   </div>
-);
-})}
-</div>
-
-{/* <div className={styles.recent_four_products_div}>
-{topFour.map((product) => {
-console.log(product);
-return <ProductCard product={product} />;
-})}
-</div> */}
-<div className={styles.recent_four_products_div}>
-{topFour.map((product) => {
-      return (
-         <div className={styles.home_product_outer_border}>
-            <article className={styles.home_product_article}>
-                  <Image width={2000} height={3000} src={product.url} alt={product.description}/>
-{/* <img src={product.url} alt={product.description} /> */}
-                  <h4 className={styles.home_product_title}>{product.product_title}</h4>
-                  <Link className={styles.home_product_link} href={"/"}>View </Link>
-            </article>
- </div>
- );
- })}
-   </div>
- </div>
-  );
- }
-
