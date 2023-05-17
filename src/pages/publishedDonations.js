15c3a18359d8be6c04a150e5ad26c4d2a0b21b84
Mojtaba-amirii <Mojtaba.a.amiri@outlook.com>
2023-05-17 13:09:00 +0200

    css under work

--- src/pages/publishedDonations.js
+++ src/pages/publishedDonations.js
@@ -1,4 +1,7 @@
 import styles from "../styles/publishedDonations.module.css";
+import { FaGreaterThan } from "react-icons/fa";
+import { FaLessThan } from "react-icons/fa";
+import { MdBookmarkAdd } from "react-icons/md";
 
 export default function PublishedDonation() {
   return (
@@ -7,15 +10,28 @@ export default function PublishedDonation() {
         Home`{">"}`Categories`{">"}`Figures`{">"}`2b action figure
       </h1>
       <section className={styles.section}>
-        <h2 className={styles.header}>2B action figure</h2>
+        <h2 className={styles.title1}>2B action figure</h2>
         <div className={styles.donatorToyLover}>
-          <h3 className={styles.donator}>Donator:</h3>
-          <p className={styles.toyLover}>TOYLOVER</p>
+          <h3 className={styles.title2}>Donator:</h3>
+          <p className={styles.title3}>TOYLOVER</p>
         </div>
       </section>
-      <img className={styles.img} />
-      <section className={styles.descriptionSection}>
-        <h2 className={styles.description}>Description</h2>
+      <section className={styles.imgSection}>
+        <label className={styles.bookMark}>
+          <MdBookmarkAdd />
+        </label>
+        <label className={styles.lessGreat}>
+          <FaLessThan />
+        </label>
+        <img src="2b2.svg" className={styles.sideImg} />
+        <img src="2b.svg" className={styles.img} />
+        <img src="2b4.svg" className={styles.sideImg} />
+        <label className={styles.lessGreat}>
+          <FaGreaterThan />
+        </label>
+      </section>
+      <section className={styles.desSection}>
+        <h2 className={styles.title1}>Description</h2>
         <p className={styles.desP}>
           2b action figure that I got from my friend, I don’t like it anymore
           and would like to give it away because me and him are not friends
@@ -23,18 +39,22 @@ export default function PublishedDonation() {
           mention it is in mint condition.{" "}
         </p>
         <div className={styles.locationMadagascar}>
-          <h3 className={styles.location}>Location:</h3>
-          <p className={styles.madagascar}>Madagascar</p>
+          <h3 className={styles.title2}>Location:</h3>
+          <p className={styles.title3}>Madagascar</p>
         </div>
-        <h2 className={styles.condition}>Condition</h2>
-        <p className={styles.poor}>Poor</p>
+        <h2 className={styles.title3}>Condition</h2>
+        <p className={styles.desP}>Poor</p>
         <h4 className={styles.good}>Good</h4>
-        <p className={styles.new}>New</p>
+        <p className={styles.desP}>New</p>
       </section>
-      <section className={styles.messageSection}>
-        <h2 className={styles.message}>Message</h2>
-        <p className={styles.writeMes}>Send a message to the donator!</p>
-        <button className={styles.button}>Send</button>
+      <section className={styles.mesSection}>
+        <h2 className={styles.title1}>Message</h2>
+        <section className={styles.writeMesSection}>
+          <p className={styles.writeMes}>Send a message to the donator!</p>
+        </section>
+        <button className={styles.button}>
+          <p className={styles.btnText}>Send</p>
+        </button>
       </section>
     </main>
   );
