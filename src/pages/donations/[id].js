d67c9d59620494494b2044f7b9c37a6642b977b3
nawrazaltai <nawraz.altai@chasacademy.se>
2023-06-12 15:00:12 +0200

    upload, donations and donation pages added

---
+++ src/pages/donations/[id].js
@@ -0,0 +1,99 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "@/styles/Donation.module.css";
+import Image from "next/image";
+
+export default function Donation() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [product, setProduct] = useState([]);
+  const [images, setImages] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [condition, setCondition] = useState("");
+
+  const conditionsArray = [
+    { id: 1, condition: "Poor" },
+    { id: 2, condition: "Good" },
+    { id: 3, condition: "New" },
+  ];
+
+  useEffect(() => {
+    async function FetchProducts() {
+      const apiUrl = "http://localhost:3000/api/donation/";
+      const postData = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: id }),
+      };
+      const response = await fetch(apiUrl, postData);
+      const res = await response.json();
+      console.log(res);
+      setProduct(res.product[0]);
+      setImages(res.images[0]);
+      setCategories(res.categories[0]);
+      setCondition(res.condition);
+    }
+
+    FetchProducts();
+  }, [router.query.id, router.isReady]);
+
+  return (
+    <div className={styles.donation_container}>
+      <div className={styles.donation_title_and_donator_div}>
+        <h2 className={styles.donation_title}>{product?.product_title}</h2>
+        <div className={styles.donator_div}>
+          <span className={styles.donator_text}>Donator:</span>{" "}
+          <span className={styles.donator_username}>
+            {product?.product_owner}
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.images_div}>
+        {images?.map((image) => {
+          return (
+            <Image
+              className={styles.donation_image}
+              src={image.image_url}
+              width={1000}
+              height={1000}
+            />
+          );
+        })}
+      </div>
+
+      <div className={styles.desc_loc_cond_div}>
+        <h3 className={styles.description_h3}>Description</h3>
+        <p className={styles.description}>{product?.product_description}</p>
+
+        <div className={styles.location_div}>
+          <span className={styles.location_text}>Location:</span>
+          <span className={styles.location}>{product?.location}</span>
+        </div>
+
+        <div className={styles.condition_div}>
+          <h3 className={styles.condition_text}>Condition</h3>
+          <ul className={styles.condition_ul}>
+            {conditionsArray.map((item) => {
+              return (
+                <li
+                  className={
+                    item.condition == condition
+                      ? styles.right_condition
+                      : styles.default_condition
+                  }
+                >
+                  {item.condition}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/donations/index.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import styles from "@/styles/Donations.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:3000/api/products", {
+    // "https://planetscale-test-navy.vercel.app/api/products"
+    // "http://localhost:3000/api/products"
+    method: "GET",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  const jsonData = await response.json();
+
+  return {
+    props: {
+      donations: jsonData.products,
+    },
+  };
+};
+
+export default function Donations({ donations }) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div className={styles.donations_container}>
+      <h1 className={styles.donations_h1}>Donations</h1>
+      {/* <div>
+        {donations.map((product) => {
+          return <div key={product.product_id}>{product.product_title}</div>;
+        })}
+      </div> */}
+      <div className={styles.donations_div}>
+        {donations.map((product) => {
+          return (
+            <div
+              key={product.product_id}
+              className={styles.home_product_outer_border}
+            >
+              <article className={styles.home_product_article}>
+                <Image
+                  width={2000}
+                  height={3000}
+                  src={product.url}
+                  alt={product.product_description}
+                  // alt="Product desc"
+                />
+                {/* <img src={product.url} alt={product.description} /> */}
+                <h4 className={styles.home_product_title}>
+                  {product.product_title}
+                </h4>
+                <Link
+                  className={styles.home_product_link}
+                  href={"/donations/" + product.product_id}
+                >
+                  View
+                </Link>
+              </article>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
