f17b7963b364c7f058ac56896b8ff95f1b7e1f01
nawrazaltai <nawraz.altai@chasacademy.se>
2023-09-22 14:00:40 +0200

    img to Next Image

--- src/pages/components/homeCards.js/firstCard.js
+++ src/pages/components/homeCards.js/firstCard.js
@@ -1,16 +1,25 @@
 import styles from "./homeCard.module.css";
 import { Lilita_One } from "next/font/google";
+import Image from "next/image";
 
+const lilitaOne = Lilita_One({ weight: "400", subsets: ["latin"] });
 
-  const lilitaOne = Lilita_One({ weight: "400", subsets: ["latin"] });
-
-
-const FirstCard = ({product}) => {
+const FirstCard = ({ product }) => {
   return (
     <div className={styles.imgContainer}>
-      <img src={product.url} alt="robot" className={styles.img} />
-      <div className={`${styles.cardtext} ${lilitaOne.className}`}>{product.product_title}</div>
-      <button  className={`${styles.buttonstyle} ${lilitaOne.className}`}>View</button>
+      <Image
+        src={product.url}
+        width={500}
+        height={500}
+        alt="robot"
+        className={styles.img}
+      />
+      <div className={`${styles.cardtext} ${lilitaOne.className}`}>
+        {product.product_title}
+      </div>
+      <button className={`${styles.buttonstyle} ${lilitaOne.className}`}>
+        View
+      </button>
     </div>
   );
 };
